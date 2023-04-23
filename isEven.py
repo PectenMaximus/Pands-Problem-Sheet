d1bf0ef9aa64f124bc3ec2847887ec7b453c4795
Alec Reid <123995169+PectenMaximus@users.noreply.github.com>
2023-04-23 13:35:49 +0100

    Add files via upload

---
+++ isEven.py
@@ -0,0 +1,6 @@
+# This programme tells the user if a number is even or odd
+# Author Alec Reid 20/04/2023
+
+number = int(input ("enter a number please"))
+if (number % 2 ) == 0: print (f"{number} is an even number")
+else: print (f"{number} is an odd number")
